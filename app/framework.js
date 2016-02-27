afd9a8ed499b76593adddab6c89b8717d3f3445d
malles <matthijs@bixie.nl>
2016-02-27 02:05:00 +0100

    move form fields to framework

--- app/framework.js
+++ app/framework.js
@@ -5,6 +5,10 @@ if (window.Vue) {
     Vue.component('input-tags', require('./components/input-tags.vue'));
     //directives
     Vue.directive('spinner', require('./directives/spinner'));
+    //partials
+    Vue.partial('fieldtype-basic', require('./templates/fieldtype-basic.html'));
+    Vue.partial('fieldtype-settings', require('./templates/fieldtype-settings.html'));
+    Vue.partial('fieldtype-appearance', require('./templates/fieldtype-appearance.html'));
     //fields
     require('./form-fields/fields')(Vue);
 
